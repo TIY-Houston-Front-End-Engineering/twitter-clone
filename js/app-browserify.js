2f4d45f8b20980feeb29ee4901f32cf76dbde508
joebobdog9 <benjamin.wilkes.howard@gmail.com>
2015-06-25 15:50:24 -0500

    Styling for tweet. Working on getting SVG and Avatar in

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -1,26 +1,48 @@
-"use strict";
 
-// es5 polyfills, powered by es5-shim
-require("es5-shim")
-// es6 polyfills, powered by babel
-require("babel/register")
+'use-strict';
+require('es5-shim');
+require('babel/register')
 
-var Promise = require('es6-promise').Promise
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
+import {Promise} from 'es6-promise'
+import $ from 'jquery'
+import Backbone from 'backbone'
+import React from 'react'
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
+Parse.initialize("OWNIhkyQCSu1aS74cx0DkdSF31EN314Vz5YDyLsd", "XkdUHPnU0KAYIXnvfcOEpQAkA1zCNDvxhKuN2maZ");
 
-// require your own libraries, too!
-// var Router = require('./app.js')
+var Bleep = Parse.Object.extend({
+    className: 'tweets',
+    defaults: {
+        handle: '',
+        bleeps: `you're so stupid that you forgot to write a message. Nice!`
+    }
+})
 
-// window.addEventListener('load', app)
+class BleepView extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render () {
+        return (<div className='bleep'>
+                    <div className='user'></div>
+                    <div className='content'></div>
+                    <div className='bar'>
+                            <div> <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+                                        viewBox="0 0 24 24" enable-background="new 0 0 24 24">
+                                        <path d="M15,3H6C5.2,3,4.5,3.5,4.2,4.2l-3,7.1C1,11.5,1,11.7,1,12v1.9l0,0L1,14c0,1.1,0.9,2,2,2h6.3l-0.9,4.6l0,0.3
+                                        c0,0.4,0.2,0.8,0.4,1.1L9.8,23l6.6-6.6C16.8,16,17,15.6,17,15V5C17,3.9,16.1,3,15,3z M19,3v12h4V3H19z"/>
+                            </svg> </div>
 
-// function app() {
-    // start app
-    // new Router()
-// }
+                            <div> <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                                        viewBox="0 0 24 24" enable-background="new 0 0 24 24">
+                                        <path d="M12,6v3l4-4l-4-4v3c-4.4,0-8,3.6-8,8c0,1.6,0.5,3,1.2,4.3l1.5-1.5C6.2,14,6,13,6,12C6,8.7,8.7,6,12,6z M18.8,7.7l-1.5,1.5
+                                        C17.7,10,18,11,18,12c0,3.3-2.7,6-6,6v-3l-4,4l4,4v-3c4.4,0,8-3.6,8-8C20,10.4,19.5,9,18.8,7.7z"/>
+                            </svg></div>
 
+
+                    </div>
+                </div>)
+    }
+}
+
+var test = new Bleep()
